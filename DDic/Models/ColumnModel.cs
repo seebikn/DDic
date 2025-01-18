ee9f525020ec30ef235a5037a7936580fd85800a
seebikn <stserver.bise@gmail.com>
2025-01-18 18:05:33 +0900

    不要なusingの削除

--- DDic/Models/ColumnModel.cs
+++ DDic/Models/ColumnModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DDic.Models
+﻿namespace DDic.Models
 {
     internal class ColumnModel
     {
